9a18ed4d5436006bc665fcc4560310d78172d472
nikhen-s <nikhen.sanjaya2003@gmail.com>
2024-09-20 16:28:55 +0100

    Set height for course card

--- uoe-course-reviews/src/CourseCard.js
+++ uoe-course-reviews/src/CourseCard.js
@@ -1,28 +1,28 @@
 import { Heading, Box, Card, CardBody, Text, Stack, Spacer} from '@chakra-ui/react'
 
-function CourseDetailsBox({courseCode, courseName, description}){
+function CourseDetailsBox({courseCode, courseName}){
     return (
     <Box>
       <Heading fontSize='xl'>{courseCode} - {courseName}</Heading>
-      <Text mt={4}>{description}</Text>
     </Box>
     )
   }
   
   function CourseRatingBox({averageRating, numberOfReviews}){ //to implement -> numberOfReviews
     return (
-    <Box bg='tomato' w='20%' borderRadius='md' p={4} color='white'>
+    <Box bg='tomato' w='10%' borderRadius='md' p={4} color='white' height={10}>
       {averageRating}
+      <Text fontSize='xs'> {numberOfReviews} reviews</Text>
     </Box>
     )
   }
   
-  function CourseCard({courseCode, courseName, description, averageRating, numberOfReviews}){
+  function CourseCard({courseCode, courseName, averageRating, numberOfReviews}){
     return(
     <Card m = {5}>
       <CardBody>
         <Stack direction={['column', 'row']} spacing='24px'>
-            <CourseDetailsBox courseCode={courseCode} courseName={courseName} description={description}></CourseDetailsBox>
+            <CourseDetailsBox courseCode={courseCode} courseName={courseName}></CourseDetailsBox>
             <Spacer />
             <CourseRatingBox averageRating={averageRating} numberOfReviews={numberOfReviews}></CourseRatingBox>
         </Stack>
