4b724a46eb5ce4cee7862ee9d9f976511dea7ae4
nikhen-s <nikhen.sanjaya2003@gmail.com>
2024-09-24 13:52:35 +0100

    Added Server Side Code and Get Courses API Route to display courses in front end

--- uoe-course-reviews/src/App.js
+++ uoe-course-reviews/src/App.js
@@ -5,16 +5,35 @@ import DesktopNavBar from './NavBar';
 import theme from './Theme'
 import SearchAndFilterBar from './SearchAndFilterBar';
 import CourseAccordionItem from './CourseAccordion';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    console.log("Use effect ran")
+    axios.get("/api/courses")
+      .then(response => {
+        setCourses(response.data)
+        console.log('Courses fetched:', response.data);
+      }
+        )
+      .catch(error => console.error(error));
+  }, []);
 
   return (
     <ChakraProvider theme={theme}>
       <DesktopNavBar></DesktopNavBar>
       <SearchAndFilterBar></SearchAndFilterBar>
-      <Accordion defaultIndex={[0]} allowMultiple>
-        <CourseAccordionItem courseCode="INFXXXX" courseName = "Computer Architecture and Design" descri
-ption = "This is a test description." averageRating="5.5" numberOfReviews="50"></CourseAccordionItem>
+      <Accordion defaultIndex={[0]} allowMultiple={true}>
+            {courses.map(course => 
+            <CourseAccordionItem courseCode={course["Code/DPT"]} courseLink ={course["DPMT_Link"]} 
+              courseName = {course["Course_Name"]} courseDelivery = {course["Delivery"]} 
+              courseWorkExamRatio={course["Work%/Exam%"]} courseCredits={course["Credits"]} 
+              courseLevel = {course["Level"]} description = "This is a test description." 
+              averageRating="5.5" numberOfReviews="50">
+            </CourseAccordionItem>)}
       </Accordion>
     </ChakraProvider>
   );
